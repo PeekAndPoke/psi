21d8c516f41bb4f6a58db5d3f95ae27588e5a786
PeekAndPoke <kontakt@karsten-gerber.de>
2015-10-09 10:02:51 +0200

    trying readthedocs

--- .provision/provision.sh
+++ .provision/provision.sh
@@ -5,10 +5,14 @@ set -o verbose
 sudo apt-get update -q
 
 # install useful things
-sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" vim htop curl mc git
+sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" vim htop curl mc git python-pip
 
 # install php and libraries
 sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" php5 php5-curl php5-intl php5-xsl php5-dev php5-cli
 
 # install debug tools
 sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" php5-xdebug
+
+
+# install sphinx for documentation
+sudo pip install sphinx sphinx-autobuild
---
+++ docs/source/index.rst
@@ -0,0 +1,22 @@
+.. PSI - Php Simplified Iterations documentation master file, created by
+   sphinx-quickstart on Fri Oct  9 08:00:20 2015.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to PSI - Php Simplified Iterations's documentation!
+===========================================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
